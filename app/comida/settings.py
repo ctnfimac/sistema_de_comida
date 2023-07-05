b72a709e7467e4b424ffb351737bfa6f14533d53
Christian Peralta <christianipc.1987@gmail.com>
2023-07-05 01:12:32 -0300

    creo una aplicacion nueva llamada web creo una vista simple para ver el home de la web y agrego los archivos estaticos css,js,img. Falta refactorizarlo

--- app/comida/settings.py
+++ app/comida/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'web',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'comida.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'], # le indico que lea en primer termino los .html en la carpeta templates
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,7 +120,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+# para que lea los archivos staticos de la carpeta static
+STATICFILES_DIRS = [
+   BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
