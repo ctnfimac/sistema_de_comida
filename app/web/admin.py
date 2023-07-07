2a7391fa3eeb13a5ba98c2e2c2ab236db5e1404e
Christian Peralta <christianipc.1987@gmail.com>
2023-07-07 00:27:49 -0300

    modelado, agrego los modelos usuario, administrador, localidad y cliente. sus correspondientes administradores pero sin configuracion y modifico el pdf del DER

--- app/web/admin.py
+++ app/web/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Usuario, Administrador, Localidad, Cliente
 
-# Register your models here.
+@admin.register(Usuario)
+class UsuarioAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Administrador)
+class AdministradorAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Localidad)
+class LocalidadAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Cliente)
+class ClienteAdmin(admin.ModelAdmin):
+    pass
--- app/web/models.py
+++ app/web/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
-# Create your models here.
+class Usuario(models.Model):
+    nombre = models.CharField(max_length=30, null=False)
+    apellido = models.CharField(max_length=30, null=False)
+    email = models.EmailField(max_length=50, null=False, unique=True)
+    contrasenia = models.CharField(max_length=50, null=False)
+    telefono = models.CharField(max_length=30, null=False)
+    habilitado = models.BooleanField(default=False)
+    cuenta_activada = models.BooleanField(default=False)
+    fecha_alta = models.DateTimeField(auto_now_add=True, blank=True)
+    fecha_activacion = models.DateTimeField(null=True, blank=True)
+    fecha_modificacion = models.DateTimeField(auto_now=True, null=True)
+
+    def __str__(self) -> str:
+        return f'{self.nombre} {self.nombre}, email: {self.email}'
+    
+
+
+class Administrador(models.Model):
+    usuario = models.OneToOneField(Usuario, on_delete=models.RESTRICT)
+    admin = models.CharField(max_length=30, null=False)
+
+    def __str__(self) -> str:
+        return f'{self.admin}.{self.usuario}'
+    
+
+
+class Localidad(models.Model):
+    descripcion = models.CharField(max_length=30, null=False)
+    fecha_alta = models.DateTimeField(auto_now_add=True, blank=True)
+    fecha_modificacion = models.DateTimeField(auto_now=True, null=True)
+
+    def __str__(self) -> str:
+        return self.descripcion
+    
+
+class Cliente(models.Model):
+    usuario = models.OneToOneField(Usuario, on_delete=models.RESTRICT)
+    localidad = models.ForeignKey(Localidad, on_delete=models.RESTRICT)
+    calle = models.CharField(max_length=30, null=False)
+    altura = models.CharField(max_length=30, null=False)
+    direccion = models.EmailField(max_length=50, null=False, unique=True)
+    
+    def __str__(self) -> str:
+        return f'{self.usuario}'
